8509db943dd43052f660311c38bc1b5f600a558c
MrAli-s <horrorstories189@gmail.com>
2021-02-28 18:18:24 +0100

    first commit

--- kitthack.sh
+++ kitthack.sh
@@ -38,5 +38,6 @@ git init
 git add .
 git commit -m "first commit"
 git remote add origin https://github.com/MrAli-s/kitthack.git
+git remote add origin https://github.com/MrAli-s/01010cam.git
 git pull --rebase origin master
 git push -u origin master
